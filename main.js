a29aee5b47e0605d4468ba8e06f3b56965331861
dougiefresh49 <dougiefresh491@gmail.com>
2023-07-26 13:45:41 -0500

    fix(api): fix api calls with updated endpoints

--- main.js
+++ main.js
@@ -1,53 +1,77 @@
-import './style.css'
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <input type="url" id="url" name="url" placeholder="Enter a URL" value="https://podnews.net/audio/podnews230721.mp3" />
   <button id="submit">Transcribe</button>
-`
+  <div id="results"></div>
+`;
 
-const baseURL = (import.meta.env.PROD) ? 'https://transcribe.fm' : 'http://localhost:8080';
+const baseURL = import.meta.env.PROD
+  ? 'https://transcribe.fm'
+  : 'http://localhost:8080';
 const url = document.querySelector('#url');
 const submit = document.querySelector('#submit');
+const sleep = (m) => new Promise((r) => setTimeout(r, m));
 
 submit.addEventListener('click', async () => {
-  if (typeof window.webln === "undefined") {
-    return alert("No WebLN available.");
+  if (typeof window.webln === 'undefined') {
+    return alert('No WebLN available.');
   }
 
   try {
     await window.webln.enable();
   } catch (error) {
-    return alert("User denied permission or cancelled.");
+    return alert('User denied permission or cancelled.');
   }
 
   try {
-    const quote = await fetch(`${baseURL}/api/v1/quote`, {
+    const response1 = await fetch(`${baseURL}/api/v1/transcribe`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ audio_url: url.value })
+      body: JSON.stringify({ audio_url: url.value }),
     });
-    // expect 402 status code
-    console.log(JSON.stringify(quote));
-    const authHeader = quote.headers.get('WWW-Authenticate');
+    // NOTE: expect 402 status code
+    const authHeader = response1.headers.get('WWW-Authenticate');
     const macaroon = authHeader.split('macaroon="')[1].split('"')[0];
     const invoice = authHeader.split('invoice="')[1].split('"')[0];
 
     const payment = await window.webln.sendPayment(invoice);
     const preimage = payment.preimage;
 
-    const transcribe = await fetch(`${baseURL}/api/v1/transcribe`, {
+    const resppnse2 = await fetch(`${baseURL}/api/v1/transcribe`, {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `L402 ${macaroon}:${preimage}`
+        Authorization: `L402 ${macaroon}:${preimage}`,
       },
-      body: JSON.stringify({ audio_url: url.value })
+      body: JSON.stringify({ audio_url: url.value }),
     });
-    const transcription = await transcribe.json();
-  }
-  catch (error) {
+
+    if (resppnse2.status === 200) {
+      const { transcript_id } = await resppnse2.json();
+      console.log(transcript_id);
+      await sleep(2000);
+      const results = await fetch(
+        `${baseURL}/transcript/${transcript_id}.json`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      const jsonResults = await results.json();
+      document.querySelector('#results').innerHTML = `<pre>${JSON.stringify(
+        jsonResults,
+        null,
+        2
+      )}</pre>`;
+    } else {
+      const err = await resppnse2.text();
+      throw new Error(err);
+    }
+  } catch (error) {
     console.log(error);
   }
 });
-
